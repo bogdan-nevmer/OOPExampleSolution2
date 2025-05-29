15c2716c5c1324ca67678b98849565f6ed0bf466
bogdan nevmer <weckman689@gmail.com>
2025-05-29 18:22:12 +0300

    update

--- PointProject/Point2D.cpp
+++ PointProject/Point2D.cpp
@@ -5,12 +5,19 @@ bool Point2D::operator==(Point2D point) {
 	return x == point.x && y == point.y;
 }
 
-bool Point2D::operator!=(Point2D point){}
+bool Point2D::operator!=(Point2D point){
+	return !(*this == point);
+}
 
-bool Point2D::operator>(Point2D point){}
+bool Point2D::operator>(Point2D point){
 
-bool Point2D::operator<(Point2D point){}
+}
+bool Point2D::operator<(Point2D point){
 
-bool Point2D::operator>=(Point2D point){}
+}
+bool Point2D::operator>=(Point2D point){
 
-bool Point2D::operator<=(Point2D point){}
+}
+bool Point2D::operator<=(Point2D point){
+
+}
